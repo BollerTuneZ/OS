6d139720c0a7cb823f1a3dcacfdb51c5b31e4aff
steffenreimann <steffenreimann2011@googlemail.com>
2016-02-15 22:01:22 +0100

    SocketIO JSON
    SocketIO angepasst das alle clients die daten bekommen nur der Sendende
    nicht! Außerdem wird nun auch die Checkbox abgeglichen.

--- public/js/btz-client.js
+++ public/js/btz-client.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 	$.getJSON("../json/test.json", function(json) {
 //SocketIO verbinden ----------------------------------------------------------------------------------------------
 		var socket = io.connect();
-
 //ON Disconnect ---------------------------------------------------------------------------------------------------
 		socket.on("disconnect", function(){
 			$('#onoffline').modal('show')
@@ -15,7 +14,7 @@ $(document).ready(function(){
 //ON json.socketname1 ---------------------------------------------------------------------------------------------
 		socket.on(json.socketname1, function(data){
 			$(json.rangeid).val(data.siodata);
-			$(json.label).html(data.siodata);
+			//$(json.label).html(data.siodata);
 		});
 //ON json.socketname2 ---------------------------------------------------------------------------------------------
 		socket.on(json.socketname2, function(data){
@@ -30,7 +29,6 @@ $(document).ready(function(){
 			console.log( "socket.emit(" + name + ", { siodata: " + siodata + "});" );
 		});
 //Inputgroup   ----------------------------------------------------------------------------------------------------
-//Click Button ----------------------------------------------------------------------------------------------------
 		$(document.body).on('click', json.checkid ,function(){
 			var chts = $(json.checkid + ':checked').val();
 			if(chts == "on"){
@@ -62,15 +60,9 @@ $(document).ready(function(){
 		});
 //mousedown setInterval -------------------------------------------------------------------------------------------
 		$(document.body).on('mousedown', json.rangeid ,function(){
-			interval = setInterval(function(){
-				$(json.label).html($(json.rangeid).val());
-				socket.emit(json.socketname1, { 'siodata': $(json.rangeid).val() });
-			},11);
 		});
 //mouseup clearInterval -------------------------------------------------------------------------------------------
 		$(document.body).on('mouseup', json.rangeid ,function(event){
-			clearInterval(interval);
-			$(json.label).html('Vorne')
 		});
 //touch -- Start --------------------------------------------------------------------------------------------------
 		$(document.body).on('touchstart', json.rangeid ,function(){
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express        = require('express');
 var app            = express();  
 var httpServer = require("http").createServer(app);  
 var conf = require('./config.json');
+var json = require('./public/json/test.json');
 var io = require('socket.io').listen(httpServer);
 
  
@@ -12,36 +13,17 @@ app.get('/', function(req, res) {
         res.sendFile(__dirname + '/public/index.html');
 });
 
- 
 httpServer.listen(conf.port);  
 console.log('Server Läuft unter http://localhost:' + conf.port);  
 
- 
-
- 
 //Socket connection handler
 io.sockets.on('connection', function (socket) {
-        console.log(socket.id);
- 
-        socket.on('test', function (data) {
-
-            console.log(data.siodata);
-            io.sockets.emit('test', { siodata: data.siodata });
-        });
-
-        socket.on('test2', function (data) {
-            console.log(data.siodata);
-            io.sockets.emit('test2', { siodata: data.siodata });
-        });
-        
-        socket.on('led:trigger', function (data) {
-            console.log('LED Trigger RECEIVED');
- 
-        });
-
-
-
-        
+    console.log(socket.id);
+    socket.on(json.group1.socketname1, function (data) {
+            socket.broadcast.emit(json.group1.socketname1, { siodata: data.siodata });
     });
- 
- 
+    socket.on(json.group1.socketname2, function (data) {
+        socket.broadcast.emit(json.group1.socketname2, { siodata: data.siodata });
+    });
+});
+
