2089f94b8230520cca6aaedd6b0c7666d95c8b37
jonas ahlf <javerik.cyber@googlemail.com>
2016-02-17 19:18:45 +0100

    git ignore

---
+++ arduino/libs/btz_i2c/btz_i2c.c
@@ -0,0 +1,22 @@
+#include "btz_i2c.h"
+
+
+
+Btz_i2c::Btz_i2c()
+{
+	
+}
+Btz_i2c::~Btz_i2c()
+{
+	
+}
+
+void Btz_i2c::Initialize(char i2c_address,char registerCount)
+{
+	
+}
+		
+char Btz_i2c::AddRegister(char i2c_register,void *value)
+{
+	
+}
