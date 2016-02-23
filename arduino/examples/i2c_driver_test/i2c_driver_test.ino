a7c20990a61cfb584f6c7a2b2bbbaf95d07fd8d2
jonas ahlf <javerik.cyber@googlemail.com>
2016-02-23 20:01:37 +0100

    +

--- arduino/examples/i2c_driver_test/i2c_driver_test.ino
+++ arduino/examples/i2c_driver_test/i2c_driver_test.ino
@@ -50,11 +50,12 @@ void loop() {
     lastTimeLogChanged = now;
   }
 }
-
+void **testArray;
 void GenRegister()
 {
     //First Register counter which will be counted in loop
-    register0[0] = new char(DT_INT); //registervalue is typeof integer
+    register0[0] = new char('h');  //registervalue is typeof integer
+    
     register0[1] = &counter; //pointer to counter;
     
     //Second register
@@ -64,5 +65,42 @@ void GenRegister()
     //Add to array
     i2c_register[0] = register0;
     i2c_register[1] = register1;
+    //Printing register setup
+    Serial.println(sizeof(i2c_register[0]));
+    void **pointer = (void**)i2c_register[0];
+    char chrValue = *(char*)pointer[0];
+    Serial.println(chrValue);
+
+  
+  int *someInt = new int(10000);
+  char *someData = "Was geht denn ab";
+  void **registerStruct;
+  registerStruct[0] = new char('H');
+  registerStruct[1] = new int(1212);
+  
+  
+  Serial.println(someData);
+  testArray[0] = someInt;
+  testArray[1] = someData;
+  testArray[2] = registerStruct;
+
+  void **arryPointer =  (void**)testArray[2];
+  char chrValue2 = *(char*)arryPointer[0];
+  Serial.println(chrValue2);
+    
+  /*
+    for(int i=0;i<2;i++)
+    {
+        void *registerPointer = i2c_register[0];
+        char *data_type = (char*)registerPointer[0];
+      char data_type2 = *(char*)registerPointer[0];
+      if(data_type2 > 0)
+      {
+        Serial.println("YO");
+      }
+        Serial.println(sizeof(data_type),DEC);
+        Serial.println(data_type2);
+        
+    }*/
 }
 
