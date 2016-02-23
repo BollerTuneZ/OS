ffbc145d1e150f091150867498e945052ae5a5fa
steffenreimann <steffenreimann2011@googlemail.com>
2016-02-23 22:41:08 +0100

    +

--- httpserver/server.js
+++ httpserver/server.js
@@ -13,11 +13,22 @@ r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     if (err) throw err;
     connection = conn;
 
-
+/*
     r.db('test').tableCreate('authors').run(connection, function(err, result) {
         if (err) throw err;
         console.log(result);
     });
+*/
+
+    r.table('config').run(connection, function(err, cursor) {
+        if (err) throw err;
+        cursor.toArray(function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        });
+    });
+
+
 });
 
 
