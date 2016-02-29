50760cdb6fe6ef72f54cfb8d9bb6bfa9571e292e
jonas ahlf <javerik.cyber@googlemail.com>
2016-02-29 23:30:03 +0100

    light dim example

---
+++ tests/rapi_arduino_btz_i2c/light.cpp
@@ -0,0 +1,54 @@
+extern "C" 
+{
+	#include "../../raspi/i2c/btz_i2c.h"
+}
+#include <stdio.h>
+#include <unistd.h>
+
+char lightRegister = 0x2,slaveAdreess = 0x12;
+char ledData[2];
+
+void DimLight(char led, char brightness)
+{
+	ledData[led] = brightness;
+	usleep(10 *1000);
+	result = WRITE_REGISTER(slaveAdreess,lightRegister,ledData,2);
+}
+
+int main()
+{
+	printf("Light dim example\n");
+	
+	printf("Initializing i2c bus\n");
+	
+	int result = INIT_I2C();
+	printf("Init result:%i\n",result);
+	
+	
+	char ledValue =0;
+	char led = 0;
+	for(int i=0;i < 255;;)
+	{
+		if(led == 0)
+		{
+			DimLight(led,ledValue);
+			i++;
+			led = 1;
+		}else
+		{
+			DimLight(led,ledValue);
+			led = 0;
+		}
+		
+		if(ledValue == 255)
+		{
+			ledValue = 0;
+		}else
+		{
+			ledValue += 42;
+		}
+	}
+	
+	return 1;
+	
+}
