a69127d7e18316e2e40afdc9050675c5c43766e7
jonas ahlf <javerik.cyber@googlemail.com>
2016-02-29 21:54:30 +0100

    example with light control

--- arduino/examples/i2c_driver_test/i2c_driver_test.ino
+++ arduino/examples/i2c_driver_test/i2c_driver_test.ino
@@ -1,15 +1,16 @@
 #include<Wire.h>
 #include <btz_i2c.h>
 
- 
+int led1 = 5,led2 = 6;
 int counter = 2000;
 long lastTimeCounterTriggered = 0;
 long lastTimeLogChanged =0;
 long lastLogLength =0;
 char mode = 'n';
+char ledStates[2];
 Btz_i2c i2c_driver = Btz_i2c();
-i2c_reg _register[2];
-i2c_reg _reg1, _reg2;
+i2c_reg _register[3];
+i2c_reg _reg1, _reg2, _reg3;
 /*I2C Bridge*/
 void i2c_onReceive(int n){i2c_driver.OnReceive(n);}
 void i2c_onRequest(){i2c_driver.OnRequest();}
@@ -21,11 +22,13 @@ void setup() {
   GenRegister();
   Serial.println("init i2c_driver");
   //Init driver with slave address 0x12, our pointers and the length of these register_array 
-  i2c_driver.Initialize(0x12,_register,0x2); 
+  i2c_driver.Initialize(0x12,_register,0x3); 
   //Setup bridge
   Wire.onRequest(i2c_onRequest);
   Wire.onReceive(i2c_onReceive);
   Serial.println("Driver Initialized");
+  ledStates[0] = 0x32;
+  ledStates[1] = 0xFF;
   
 }
 
@@ -35,7 +38,6 @@ void loop() {
   if((now -  lastTimeLogChanged) >= 50)
   {
     counter++;
-    Serial.println(counter);
     if(i2c_driver.log.length() != lastLogLength)
     {
       //Serial.println(i2c_driver.log);
@@ -43,6 +45,9 @@ void loop() {
     }
     lastTimeLogChanged = now;
   }
+  analogWrite(led1,ledStates[0]);
+  analogWrite(led2,ledStates[1]);
+  
 }
 void **testArray;
 void GenRegister()
@@ -52,9 +57,12 @@ void GenRegister()
 
   _reg2.dataType = DT_BYTE;
   _reg2.valuePointer = &mode;
+  
+  _reg2.dataType = DT_CHRARY;
+  _reg2.valuePointer = ledStates;
 
   _register[0] = _reg1;
   _register[1] = _reg2;
-  
+  _register[2] = _reg3;
 }
 
--- tests/rapi_arduino_btz_i2c/main.cpp
+++ tests/rapi_arduino_btz_i2c/main.cpp
@@ -45,8 +45,22 @@ int main()
 	
 	printf("Read Done.\n");
 	
+	usleep(10*1000);
 	
+	/*Set Led 1 on half power and 2 seconds later off* ,led 2 will be 100% */
+	char ledData[2];
+	ledData[0] = 0x32
+	ledData[1] = 0xFF
 	
+	result = WRITE_REGISTER(0x12,0x3,ledData,2);
+	printf("Read result:%i\n",result);	
+	usleep(( 2 *1000) * 1000);
+	
+	ledData[0] = 0x00
+	ledData[1] = 0xFF
+	
+	result = WRITE_REGISTER(0x12,0x3,ledData,2);
+	printf("Read result:%i\n",result);	
 	return 1;
 	
 }
