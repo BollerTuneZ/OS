b1cdf0621f06d6ee536795c1576d94355a96656f
jonas ahlf <javerik.cyber@googlemail.com>
2016-03-09 19:05:43 +0100

    test cleaned

--- tests/rapi_arduino_btz_i2c/main.cpp
+++ tests/rapi_arduino_btz_i2c/main.cpp
@@ -5,6 +5,14 @@ extern "C"
 #include <stdio.h>
 #include <unistd.h>
 
+#define SLAVE_ADDR 0x12
+#define REGISTER_INT 0x0
+#define REGISTER_CHAR 0x1
+#define REGISTER_CHRARRY 0x2
+
+int test_read_register();
+int test_write_register();
+int test_write_array();
 
 
 int main()
@@ -15,6 +23,14 @@ int main()
 	
 	int result = INIT_I2C();
 	printf("Init result:%i\n",result);
+	printf("Test: read register\n");
+	printf("Ended with%i\n",test_read_register());
+	printf("Test: write register\n");
+	printf("Ended with%i\n",test_write_register());
+	printf("Test: write array register\n");
+	printf("Ended with%i\n",test_write_array());
+	
+	return 1;
 	printf("try read register 0 from slave...\n");
 
 	/*
@@ -62,5 +78,86 @@ int main()
 	result = WRITE_REGISTER(0x12,0x2,ledData,2);
 	printf("Read result:%i\n",result);	
 	return 1;
+	
+}
+
+/*
+	Testing read one register from slave
+*/
+int test_read_register()
+{
+	int result;
+	char rawValue[2];
+	
+	result = READ_REGISTER(SLAVE_ADDR,REGISTER_INT,rawValue,2);
+	print_result(result);
+	if(result != 1){return -1;}
+	
+	int intVal = (rawValue[0] | rawValue[1] << 8);
+	if(intVal == 999){return 1;}
+	return -1
+}
+
+/*
+	Testing write one register
+*/
+int test_write_register()
+{
+	int result;
+	char intWriteVal[2] = {0x0,0x0};
+	result = WRITE_REGISTER(SLAVE_ADDR,REGISTER_INT,intWriteVal,4);
+	print_result(result);
+	if(result != 1){return -1;}
+	
+	char rawValue[2];
+	
+	result = READ_REGISTER(SLAVE_ADDR,REGISTER_INT,rawValue,2);
+	print_result(result);
+	if(result != 1){return -1;}
+	
+	int intVal = (rawValue[0] | rawValue[1] << 8);
+	if(intVal == 0){return 1;}
+	return -1
+}
+
+/*
+	Testing write char array register
+*/
+int test_write_array()
+{
+	int result;
+	char testData[6];
+	testData[0] = 0x1;
+	testData[1] = 0x2;
+	testData[2] = 0x3;
+	testData[3] = 0x4;
+	testData[4] = 0x5;
+	testData[5] = 0x6;
+	
+	result = WRITE_REGISTER(SLAVE_ADDR,REGISTER_CHRARRY,testData,6);
+	print_result(result);
+	if(result != 1){return -1;}
+	
+	char readVal[6];
+	
+	result = READ_REGISTER(SLAVE_ADDR,REGISTER_CHRARRY,readVal,6);
+	print_result(result);
+	if(result != 1){return -1;}
+	
+	for(int i=0;i<6;i++)
+	{
+		if(readVal[i] != testData[i])
+			return -1;
+	}
+	return -1
+}
+void print_result(int *val)
+{
+	if(*val != 1)
+	{
+	  printf("ERROR :%i\n",val);
+	  return;
+	}
+	printf("Result is:%i\n",val);
 	
 }
