8019b9afa88a858c9eefe7cb11b7f9d9ffb5c93b
javerik <jonas@juanah.de>
2016-03-13 20:16:21 +0100

    main added

---
+++ Core/serv/main.cpp
@@ -0,0 +1,55 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 13.03.2016
+ *      Author: jonas
+ */
+
+#define VERSION "0.1.0"
+
+#include <stdio.h>
+#include "../driver/driver_common.h"
+#include "../driver/i2c_base_cpp/i2c_base.h"
+#include "../driver/SoftDriver/light/light.h"
+
+
+i2c_base _i2cBaseDriver;
+Light *_lightDriver;
+
+int INIT();
+
+int main(int argc, char **argv) {
+	printf("BollerTuneZ Core Server %s\n",VERSION);
+	printf("Initializing Hardware Driver:\ni2c_base ,Light\n");
+	int initResult = INIT();
+	if(initResult != 1)
+	{
+		return initResult;
+	}
+	printf("Testing Light driver\n");
+	int frequenzy = 2500;
+
+	btz_i2c_device lightDevice;
+	lightDevice.addr = 0x40;
+	lightDevice.name = "light1";
+	light_control control;
+	control.device = &lightDevice;
+	control.ledNum = 5;
+	control.frequenzy = &frequenzy;
+	_lightDriver->SetLight(&control);
+
+
+
+}
+
+int INIT()
+{
+	int result = _i2cBaseDriver.Init();
+	if(result != 1)
+	{
+		printf("Error could not initialize i2c driver:%i\n",result);
+		return -1;
+	}
+	_lightDriver = new Light(&_i2cBaseDriver);
+	return 1;
+}
