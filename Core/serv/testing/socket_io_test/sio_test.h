4dc905dc5cc4399e948daeecf0788bfef613bee2
jonas ahlf <javerik.cyber@googlemail.com>
2016-03-14 21:17:28 +0100

    sio test code

---
+++ Core/serv/testing/socket_io_test/sio_test.h
@@ -0,0 +1,20 @@
+/*
+ * sio_test.h
+ *
+ *  Created on: 14.03.2016
+ *      Author: Jonas
+ */
+
+#ifndef SOCKET_IO_TEST_SIO_TEST_H_
+#define SOCKET_IO_TEST_SIO_TEST_H_
+
+class SIO_Test {
+public:
+
+
+private:
+};
+
+
+
+#endif /* SOCKET_IO_TEST_SIO_TEST_H_ */
