4a8234fe7c96bdffdef60790cb15b49d66034eeb
jonas ahlf <javerik.cyber@googlemail.com>
2016-03-15 22:32:29 +0100

    huge DUMP

--- Core/serv/testing/socket_io_test/sio_test.cpp
+++ Core/serv/testing/socket_io_test/sio_test.cpp
@@ -11,16 +11,17 @@ void SIO_Test::SimpleTest() {
 
 	usleep(500*1000);
 	string test_var = "{\"val\":\"hallo welt!\"}";
-	for(int i=0;i<100;i++)
-	{
-		h.socket()->emit("hello",string_message::create(test_var), [&](message::list const& msg){});
-		usleep(100*1000);
-	}
 	h.socket()->on("response_node", sio::socket::event_listener_aux([&](string const& name, message::ptr const& data, bool isAck,message::list &ack_resp){
 		string xo = "{\"val\":\"Ficken!\"}";
 		printf("Triggered\n");
 		h.socket()->emit("hello",string_message::create(test_var), [&](message::list const& msg){});
     }));
+	for(int i=0;i<100;i++)
+	{
+		h.socket()->emit("hello",string_message::create(test_var), [&](message::list const& msg){});
+		usleep(100*1000);
+	}
+
 	while(true){}
 }
 /*
