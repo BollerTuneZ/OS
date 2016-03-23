518468cd896bf90d13f6554c37b360b81927485a
jonas@juanah.de <jonas@juanah.de>
2016-03-23 19:41:13 +0100

    bla

--- Core/driver/gpio_driver/build.sh
+++ Core/driver/gpio_driver/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ -c -o  gpio.o gpio.cpp -L/usr/local/lib -lwiringPi -lwiringPiDev -lpthread -lm
+g++ -c -o  gpio_btz.o gpio_btz.cpp -L/usr/local/lib -lwiringPi -lwiringPiDev -lpthread -lm
