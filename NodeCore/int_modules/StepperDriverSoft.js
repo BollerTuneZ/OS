2109cd6c470168006ece991537562504995d1cef
jonas@juanah.de <jonas@juanah.de>
2016-05-01 00:07:07 +0200

    ada

--- NodeCore/int_modules/StepperDriverSoft.js
+++ NodeCore/int_modules/StepperDriverSoft.js
@@ -1,21 +1,114 @@
-
+var SerialPort = require("serialport").SerialPort;
+var port;
 var initCommand = 'RUN';
+var initialized = false;
 
-var states = 
+module.exports =
 {
-	gc_ok:0x10,
-	gc_fault:0x20,
-	gc_buff_overflow:0x21
+	Initialize:Initialize,
+	Drive:Drive
 };
 
-var SerialPort = require("serialport").SerialPort;
+var gc =
+{
+	move:'M',
+	dir:'D',
+	feedrate:'F'
+};
+var lastDirection = 'X';
+var lastFeedrate = 0;
 
-var serialport = new SerialPort("/dev/ttyUSB0");
+var gc_status =
+{
+	ok:0x10,
+	fault:0x20,
+	buf_overflow:0x21
+};
 
-var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyUSB0", {
-  baudrate: 9600
-});
+
+function Initialize(baudRate,callback)
+{
+	port = new SerialPort("/dev/ttyUSB0", {
+	  baudrate: baudRate
+	});
+	port.on('open',function()
+	{
+		port.write(initCommand,function(e,b){
+			if (err) {
+				console.log('Error: ', err.message);
+			}
+		});
+		port.on('data',function(data)
+		{
+			if(data == states.gc_ok)
+			{
+				initialized = true;
+				callback(true);
+			}else {
+				callback(false);
+			}
+		});
+	})
+}
+
+/* Dir : 'L' || 'R'
+*
+*/
+function Drive(steps,dir,feedrate,callback)
+{
+	if(!initialized){return -1;}
+	var buffer = "";
+	var dirState = false,feedrateState=false;
+	if(dir != lastDirection)
+	{
+		buffer += gc.dir + dir + "E";
+		port.on('data',function(data)
+		{
+			lastDirection = dir;
+			if(data == states.gc_ok)
+			{
+				dirState = true;
+			}
+		});
+		port.write(buffer);
+	}else {
+		dirState = true;
+	}
+
+	while(!dirState){}
+	buffer = "";
+	if(feedrate != lastFeedrate)
+	{
+		buffer += gc.feedrate + feedrate + "E";
+		port.on('data',function(data)
+		{
+			if(data == states.gc_ok)
+			{
+				feedrateState = true;
+			}
+		});
+		port.write(buffer);
+	}
+	while(!feedrateState){}
+	buffer = "";
+	buffer += gc.feedrate + feedrate + "E";
+	port.on('data',function(data)
+	{
+		if(data == states.gc_ok)
+		{
+			if(callback != undefined)
+			{
+				callback(true);
+			}else {
+				callback(false);
+			}
+		}
+	});
+	port.write(buffer);
+
+}
+
+
 
 serialPort.on('data', function (data) {
 	if(data == states.gc_ok)
@@ -44,7 +137,5 @@ function writeToSlave()
       return console.log('Error: ', err.message);
     }
     console.log(bytesWritten, 'bytes written');
-  });	
+  });
 }
-
-
