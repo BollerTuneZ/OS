cac57602d50ff0461de71e6c90eaae7650408ae7
jonas <jonas.ahlf@gmail.com>
2016-05-01 22:39:21 +0200

    AUTOCOMMIT_UBUNTU

--- NodeCore/int_modules/StepperDriverSoft.js
+++ NodeCore/int_modules/StepperDriverSoft.js
@@ -106,7 +106,7 @@ function GenReceive(data)
 			console.log("Ready");
 			onBusyChanged(true);
 		}
-	}else if(data.trim() == gc_status.busy)
+	}else if((data.toString().trim()) == gc_status.busy)
 	{
 		if(onBusyChanged != undefined)
 		{
