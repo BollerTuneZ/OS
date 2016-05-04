1fa02e8d9c585d92facc3c210e1f6855c3292d29
jonas <jonas.ahlf@gmail.com>
2016-05-04 17:20:30 +0200

    AUTOCOMMIT_UBUNTU

--- NodeCore/int_modules/gamepad.js
+++ NodeCore/int_modules/gamepad.js
@@ -22,7 +22,7 @@ function SetState(onOff)
 
 function _posSteeringChanged(position)
 {
-  if(!currentState || position.x == undefined || position.x == 'NaN'|| position.x == 'none'){return;}
+  if(!currentState || isNaN(position.x) || position.x == 'NaN'|| position.x == 'none'){return;}
   position = position.x;
   console.log("Controller Position:" + position);
   var mapped = btzMath.MapVal(position,0,255,config.range.min,config.range.max);
