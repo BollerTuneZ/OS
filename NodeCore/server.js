34b980b6a9042a11c807031d9e503aee5a597794
jonas <jonas.ahlf@gmail.com>
2016-05-04 10:04:19 +0200

    AUTOCOMMIT_UBUNTU

--- NodeCore/server.js
+++ NodeCore/server.js
@@ -47,7 +47,7 @@ stdin.addListener("data", function(d) {
       strCali.Initialize(initCalObj);
     }else if(input == "-drive cali")
     {
-      strCali.RefDrive(10,'L');
+      strCali.RefDrive(100,'L');
     }else {
       console.log("Could not understand:" + input);
     }
